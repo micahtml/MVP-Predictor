1537dadd742e321011527b534e4bac48ab5f1fbd
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-03-15 17:26:13 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# NBA MVP Performance Based Predictive Model
+
+This is a Python program that analyzes the performance of NBA players and predicts the top MVP candidates based on their stats. It uses the pandas library to scrape NBA season stat leaders data from basketball-reference.com, and then performs data cleaning and exploratory data analysis using Plotly to create interactive charts.
+
+## How it works
+
+1. **Data Acquisition:** The program collects NBA season stat leaders data from basketball-reference.com using pandas to read the HTML table into a DataFrame.
+
+2. **Data Cleaning:** The program cleans the data by removing unnecessary columns, renaming columns, removing duplicates, and converting relevant columns to numeric data type.
+
+3. **Exploratory Data Analysis:** The program performs exploratory data analysis using Plotly to create interactive charts that display top 5 MVP candidates based on their points, rebounds, assists, games played, and minutes played.
+
+4. **MVP Performance Prediction:** The program predicts the top MVP candidates based on their stats using a scoring system that adds up their points, rebounds, and assists, minutes & games played.
+
+## How to use it
+
+To use this program, you can follow these steps:
+
+1. Clone the repository to your local machine.
+
+2. Open the terminal and navigate to the cloned repository.
+
+3. Install the required Python libraries using pip: Pandas and Matplotlib.
+
+4. Run the main script "main.py" to perform the data analysis and generate the visualizations.
+
+5. To customize the analysis, modify the code in the "main.py" script.
+
+That's it! You can now use this program to analyze the performance of NBA players and predict the top MVP candidates based on their stats.
---
+++ main..py
@@ -0,0 +1,47 @@
+import pandas as pd
+import plotly.graph_objs as go
+
+# Define the URL for the NBA season stat leaders data
+url = 'https://www.basketball-reference.com/leagues/NBA_2023_totals.html'
+
+# Use pandas to read the HTML table into a DataFrame
+df = pd.read_html(url)[0]
+
+# Remove the "Rk" column
+df.drop('Rk', axis=1, inplace=True)
+
+# Rename the "Unnamed: 1" column to "Player"
+df.rename(columns={'Unnamed: 1': 'Player'}, inplace=True)
+
+# Remove the last 2 rows (total and league average)
+df.drop(df.tail(2).index, inplace=True)
+
+# Remove duplicates
+df.drop_duplicates(inplace=True)
+
+# Remove any rows where all columns are empty
+df.dropna(how='all', inplace=True)
+
+# Convert the relevant columns to numeric data type
+numeric_cols = ['G', 'MP', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA',
+                'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
+df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
+
+# Select the top 5 MVP candidates based on points, rebounds, assists, games played, and minutes played
+df['MVP_Score'] = df['PTS'] + df['TRB'] + df['AST']
+mvp_df = df[['Player', 'Pos', 'Age', 'Tm', 'G', 'MP', 'PTS', 'TRB', 'AST', 'MVP_Score']].sort_values('MVP_Score', ascending=False).head(5)
+
+print('Top 5 Candidates in NBA 2023 season (Performance based)')
+print(mvp_df)
+
+# Create a bar chart of the top 5 MVP candidates with their stats
+fig = go.Figure()
+fig.add_trace(go.Bar(name='Minutes Played', x=mvp_df['Player'], y=mvp_df['MP']))
+fig.add_trace(go.Bar(name='Points', x=mvp_df['Player'], y=mvp_df['PTS']))
+fig.add_trace(go.Bar(name='Rebounds', x=mvp_df['Player'], y=mvp_df['TRB']))
+fig.add_trace(go.Bar(name='Assists', x=mvp_df['Player'], y=mvp_df['AST']))
+fig.add_trace(go.Bar(name='Games Played', x=mvp_df['Player'], y=mvp_df['G']))
+
+
+fig.update_layout(title='Top 5 MVP Candidates in NBA 2023 Season (Performance based)', xaxis_title='Player', yaxis_title='Stats', barmode='group')
+fig.show()
