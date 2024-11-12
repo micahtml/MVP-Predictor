6a12a9136521d2908a1d5b0ca71cdfff4074691c
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2024-11-12 14:20:14 -0600

    "Uncommented import statements for pandas and plotly.graph_objs"

--- main_win.py
+++ main_win.py
@@ -1,5 +1,5 @@
-# import pandas as pd
-# import plotly.graph_objs as go
+import pandas as pd
+import plotly.graph_objs as go
 
 # Define the URL for the NBA season stat leaders data
 url = "https://www.basketball-reference.com/leagues/NBA_2025_totals.html"
