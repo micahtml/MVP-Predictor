b977a7ab741ce3b896c26f9d5baa83d3afa60eaf
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2024-12-16 14:49:15 -0600

    `Removed commented out code and updated comments to reflect actual code changes`

--- main_win.py
+++ main_win.py
@@ -1,48 +1,48 @@
-import pandas as pd
-import plotly.graph_objs as go
+# import pandas as pd
+# import plotly.graph_objs as go
 
-# Define the URL for the NBA season stat leaders data
-url = "https://www.basketball-reference.com/leagues/NBA_2025_totals.html"
+# # Define the URL for the NBA season stat leaders data
+# url = "https://www.basketball-reference.com/leagues/NBA_2025_totals.html"
 
-# Use pandas to read the HTML table into a DataFrame
-df = pd.read_html(url)[0]
+# # Use pandas to read the HTML table into a DataFrame
+# df = pd.read_html(url)[0]
 
-# Remove the "Rk" column
-df.drop("Rk", axis=1, inplace=True)
+# # Remove the "Rk" column
+# df.drop("Rk", axis=1, inplace=True)
 
-# Rename the "Unnamed: 1" column to "Player"
-df.rename(columns={"Unnamed: 1": "Player"}, inplace=True)
+# # Rename the "Unnamed: 1" column to "Player"
+# df.rename(columns={"Unnamed: 1": "Player"}, inplace=True)
 
-# Remove the last 2 rows (total and league average)
-df.drop(df.tail(2).index, inplace=True)
+# # Remove the last 2 rows (total and league average)
+# df.drop(df.tail(2).index, inplace=True)
 
-# Remove duplicates
-df.drop_duplicates(inplace=True)
+# # Remove duplicates
+# df.drop_duplicates(inplace=True)
 
-# Remove any rows where all columns are empty
-df.dropna(how="all", inplace=True)
+# # Remove any rows where all columns are empty
+# df.dropna(how="all", inplace=True)
 
-# Convert the relevant columns to numeric data type
-numeric_cols = [
-    "G",
-    "MP",
-    "FG",
-    "FGA",
-    "3P",
-    "3PA",
-    "FT",
-    "FTA",
-    "ORB",
-    "DRB",
-    "TRB",
-    "AST",
-    "STL",
-    "BLK",
-    "TOV",
-    "PF",
-    "PTS",
-]
-df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors="coerce")
+# # Convert the relevant columns to numeric data type
+# numeric_cols = [
+#     "G",
+#     "MP",
+#     "FG",
+#     "FGA",
+#     "3P",
+#     "3PA",
+#     "FT",
+#     "FTA",
+#     "ORB",
+#     "DRB",
+#     "TRB",
+#     "AST",
+#     "STL",
+#     "BLK",
+#     "TOV",
+#     "PF",
+#     "PTS",
+# ]
+# df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors="coerce")
 
 # Select the top 5 MVP candidates based on points, rebounds, assists, games played, and minutes played
 df["MVP_Score"] = df["PTS"] + df["TRB"] + df["AST"] + df["G"] + df["FGA"]
